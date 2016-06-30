024f1ce487c105aa78c4daa0ffa5e48da2a2afe4
Osman Hernandez <osman.sist@gmail.com>
2016-06-29 21:05:18 -0600

    user edit done

--- XOF/imports/ui/containers/users/userGridContainer.js
+++ XOF/imports/ui/containers/users/userGridContainer.js
@@ -6,7 +6,6 @@ import UserGrid from '../../components/users/user-grid.jsx';
 const composer = (props, onData) => {
 	const userHandle = Meteor.subscribe('allUsers');
 	if (userHandle.ready()) {
-		console.log('fuck');
 		const users = Meteor.users.find({}).fetch();
 		const userToEdit = new ReactiveVar({});
 		onData(null, {
