921d63dda6523500cfc130bf6a79429832765a1d
justrdk <osman.sist@gmail.com>
2016-07-17 18:59:58 -0600

    fixes

--- XOF/imports/startup/server/fixtures.js
+++ XOF/imports/startup/server/fixtures.js
@@ -1,10 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Categories } from '../../api/categories/categories';
-<<<<<<< HEAD
-=======
 import { Courses } from '../../api/courses/courses';
->>>>>>> 95410d2... list courses
 
 const initUsers = () => ([{
 	email: 'test@test.com',
@@ -30,18 +27,6 @@ const initUsers = () => ([{
 }]);
 
 const initCategories = () => ([{
-	name: 'Security',
-	description: 'Cyber Security Category',
-<<<<<<< HEAD
-}, {
-	name: 'React',
-	description: 'React Development',
-}, {
-	name: 'Meteor',
-	description: 'Meteor Development',
-=======
-	isActive: true,
-}, {
 	name: 'React',
 	description: 'React Development',
 	isActive: true,
@@ -63,16 +48,12 @@ const initCourses = () => ([{
 	title: 'Reactive programming',
 	description: 'Advanced course using RXJS',
 	isActive: true,
->>>>>>> 95410d2... list courses
 }]);
 
 Meteor.startup(() => {
 	const users = initUsers();
 	const categories = initCategories();
-<<<<<<< HEAD
-=======
 	const courses = initCourses();
->>>>>>> 95410d2... list courses
 
 	if (Meteor.users.find().count() === 0) {
 		users.forEach((user) => Accounts.createUser(user));
@@ -81,11 +62,8 @@ Meteor.startup(() => {
 	if (Categories.find().count() === 0) {
 		categories.forEach((category) => Categories.insert(category));
 	}
-<<<<<<< HEAD
-=======
 
 	if (Courses.find().count() === 0) {
 		courses.forEach((course) => Courses.insert(course));
 	}
->>>>>>> 95410d2... list courses
 });
